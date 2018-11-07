4f14ea314cfa92464168679f6081f3ecd98c76f6
OneThatWalks <onethatwalks@aol.com>
2018-11-07 08:04:18 -0500

    removed lots of html from game class.

--- src/renderer.ts
+++ src/renderer.ts
@@ -8,62 +8,33 @@ import * as path from 'path';
 
 let container,
     settings: any,
-    canvas: JQuery,
-    startButton: JQuery,
-    stopButton: JQuery,
     game: IGame;
 
-$(document).ready(() => {
+$(document).ready(async () => {
     container = $('#container');
 
-    readSettings();
+    const set = await readFile('settings.json');
+    settings = JSON.parse(set);
 
     if (container) {
-        init(container);
+        init(container, settings);
     }
 });
 
-async function readSettings() {
-    fs.readFile(path.join(__dirname, 'assets', 'settings.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        };
-        settings = JSON.parse(data);
-
-        console.log(settings);
+async function readFile(file: string, opt = 'utf8'): Promise<any> {
+    return new Promise((res, rej) => {
+        fs.readFile(path.join(__dirname, 'assets', file), opt, (err, data) => {
+            if (err) {
+                rej(err);
+            } else {
+                res(data);
+            }
+        });
     });
 }
 
 
-function init(selector: JQuery): void {
-    const row1 = $('<div class=\"row\"></div>');
-
-    startButton = $('<button id=\"startButton\" type=\"button\" class=\"btn btn-default\">Start</button>');
-    stopButton = $('<button id=\"stopButton\" type=\"button\" class=\"btn btn-default\">Stop</button>');
-
-    row1.append(startButton);
-    row1.append(stopButton);
-
-    const row2 = $('<div class=\"row\"></div>');
-
-    canvas = $('<canvas id=\"myCanvas\" width=\"320\" height=\"240\" style=\"border: 1px solid #000000\"></canvas>'); // todo: move this to a class
-
-    row2.append(canvas);
-
-    selector.append(row1);
-    selector.append(row2);
-
-    stopButton.on('click', async () => {
-        if (game) {
-            game.stop();
-        }
-    });
-
-    startButton.on('click', async () => {
-        if (!game) {
-            game = new Game();
-        }
-        game.start();
-    });
+function init(selector: JQuery, settings: any): void {
+    game = new Game(selector, settings);
 }
 
--- src/sandbox/game.ts
+++ src/sandbox/game.ts
@@ -4,34 +4,31 @@ import { IGame } from '../interfaces/igame';
 import { IGameManager } from './../interfaces/igame-manager';
 import { IGameRenderer } from './../interfaces/igame-renderer';
 import { delay } from '../util/util';
-import * as $ from 'jquery';
 
+
+/**
+ * Creates a 'Game' which is meant to be a singleton
+ *
+ * @export
+ * @class Game
+ * @implements {IGame}
+ */
 export class Game implements IGame {
-
-    readonly UP_HEADING: number = 0;
-    readonly DOWN_HEADING: number = 180;
-    readonly LEFT_HEADING: number = 270;
-    readonly RIGHT_HEADING: number = 90;
 
     private _running: boolean;
     private _delta: number;
     private _manager: IGameManager;
     private _renderer: IGameRenderer;
-    private _div: JQuery;
-    private _informationSpan: JQuery;
-    private _nextHeading: number;
-    private _nextSpaces: number;
 
     /**
      * Creates a game
      */
-    constructor(delta: number = 500) {
+    constructor(container: JQuery, settings: any, delta: number = 500) {
         this._running = false;
-        this._delta = delta;
+        this._delta = settings.game.delta !== undefined ? settings.game.delta : delta;
         this._manager = new GameManager();
         this._renderer = new GameRenderer();
 
-        this._createElements();
         this._registerEvents();
     }
 
@@ -41,7 +38,7 @@ export class Game implements IGame {
         }
 
         window.onkeydown = (event) => {
-            //this._processInput(event);
+            this._processInput(event);
         }
     }
 
@@ -49,55 +46,12 @@ export class Game implements IGame {
         console.log(event);
 
         const keyCode = event.key;
-
-        switch (keyCode) {
-            case 'w':
-                this._applyHeading(this.UP_HEADING);
-                break;
-            case 's':
-                this._applyHeading(this.DOWN_HEADING);
-                break;
-            case 'a':
-                this._applyHeading(this.LEFT_HEADING);
-                break;
-            case 'd':
-                this._applyHeading(this.RIGHT_HEADING);
-                break;
-        }
-
-    }
-
-    _applyHeading(heading: number) {
-        if (!this._isAdjacentDirection(heading, this._nextHeading)) {
-            this._nextSpaces = 0;
-        }
-        // todo
-    }
-
-    _isAdjacentDirection(newHeading: number, currentHeading: number): boolean {
-        const headerMaxDegrees = newHeading + 90;
-        const headerMinDegrees = newHeading + 90;
-        return false;
-        if (headerMaxDegrees){} // todo if in the range of the current heading we are adjacent
-    }
-
-    _createElements(): void {
-        if (!this._div && $('#game').length === 0) {
-            this._div = $('<div id=\"game\">Game: </div>');
-            this._informationSpan = $('<span id=\"gameInformation\"></span>');
-            this._div.append(this._informationSpan);
-            $('body').append(this._div);
-        } else {
-            this._div = $('#game');
-            this._informationSpan = $('#gameInformation');
-        }
     }
 
     /**
      * Starts game
      */
     async start() {
-        this._informationSpan.html('Running');
         console.log('Starting loop.');
         this._running = true;
 
@@ -114,7 +68,6 @@ export class Game implements IGame {
      * Stops game
      */
     stop(): void {
-        this._informationSpan.html('Stopped');
         this._running = false;
     }
 
