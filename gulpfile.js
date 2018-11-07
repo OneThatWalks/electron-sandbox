e39c463524a4ee21183be5af8212de4128a5b9d7
OneThatWalks <onethatwalks@aol.com>
2018-11-07 06:56:15 -0500

    Move index out of root

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('clean', function () {
 });
 
 gulp.task('copy', function () {
-    return gulp.src('./index.html')
+    return gulp.src('./src/index.html')
         .pipe(gulp.dest('./dist/'));
 })
 
